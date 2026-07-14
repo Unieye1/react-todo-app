391250ae3b15eacaea022f0d8c6923fde6b79fa3
Unieye <144839259+Unieye1@users.noreply.github.com>
2026-07-15 02:28:36 +0530

    Added PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-  ],
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Task Master',
+        short_name: 'TaskMaster',
+        description: 'Todo App with Dark Mode and Local Storage',
+        theme_color: '#1e1b4b',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ]
 })
