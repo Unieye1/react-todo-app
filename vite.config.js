b05962722cb98bf3e1a3aea1bd6aa32e1b04ab95
Unieye <144839259+Unieye1@users.noreply.github.com>
2026-07-15 03:03:14 +0530

    Added PWA icons

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,33 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
 
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       manifest: {
-        name: 'Task Master',
-        short_name: 'TaskMaster',
-        description: 'Todo App with Dark Mode and Local Storage',
-        theme_color: '#1e1b4b',
-        background_color: '#ffffff',
-        display: 'standalone',
+        name: "Task Master",
+        short_name: "TaskMaster",
+        description: "Todo App with Dark Mode and Local Storage",
+        theme_color: "#1e1b4b",
+        background_color: "#ffffff",
+        display: "standalone",
         icons: [
           {
-            src: '/vite.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml'
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: '/vite.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml'
-          }
-        ]
-      }
-    })
-  ]
-})
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
